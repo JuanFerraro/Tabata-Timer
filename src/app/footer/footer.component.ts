4fe939181fae8a5fbfdaffaf5ef23957cb79259c
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2024-01-29 11:49:00 -0500

    Modified footer Component, html and css -> src/app/footer; Added font app css.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,17 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  crewUrl: string = './assets/whiteJustCrew.png'
+  githubUrl: string = './assets/github.png'
+  linkedinUrl: string = './assets/linkedin.png'
 
 }
