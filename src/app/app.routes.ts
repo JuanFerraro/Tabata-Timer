abf12d0a8269e8da15f85e64e31a86d0580d7a2a
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2024-01-29 09:08:57 -0500

    Added timer and contact routes -> src/app/app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TimerComponent } from './timer/timer.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'timer', component: TimerComponent },
+  { path: 'contact', component: ContactComponent },
+];
