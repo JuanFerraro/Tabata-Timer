3e8408137a3574aa6836f6ee34383f73513d504c
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2024-02-04 09:37:55 -0500

    Modified contact Component -> src/app/contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  githubUrl: string = './assets/github-black.png'
 }
